bdbdfcfb73bdeb917ad22aa6288dd5ff44175c79
Sros27 <84526104+SaraRosad@users.noreply.github.com>
2022-11-13 10:55:53 -0400

    11-13-2022

--- js/script.js
+++ js/script.js
@@ -6,3 +6,15 @@ const CANVAS_HEIGHT = canvas.height = 600;
 
 const playerImage = new Image();
 playerImage.src = 'images/shadow_dog.png';
+const spriteWidth = 575;
+const spritHeight = 523;
+
+function animate(){
+    ctx.clearRect(0,0, CANVAS_WIDTH, CANVAS_HEIGHT);
+    //ctx.fillRect(100,50,100,100);
+    //ctx.drawImage(image, sx, st, sw, sh, dx, dy, dw, dh);
+    ctx.drawImage(playerImage, 0, 0, spriteWidth, spritHeight, 0, 0, spriteWidth, spritHeight);
+    requestAnimationFrame(animate);
+}
+
+animate(); 
