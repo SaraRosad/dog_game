f5246b7bba2d8799ad50cd486336790c13d3490a
Sros27 <84526104+SaraRosad@users.noreply.github.com>
2022-11-13 16:15:05 -0400

    11-13-2022

--- Npc_movements/js/enemy3_v2.js
+++ Npc_movements/js/enemy3_v2.js
@@ -9,29 +9,37 @@ let numberOfEnemies = 0;
 const enemiesArray = [];
 
 let gameFrame = 0;
+
 class Enemy{
     constructor(){
         this.image = new Image();
-        this.image.src = 'images/enemies/enemy2.png';
+        this.image.src = 'images/enemies/enemy3.png';
         this.speed = Math.random() * 4 + 1;
-        this.spriteWidth= 266;
-        this.spriteHeight = 188;
-        this.width = this.spriteWidth / 2.5;
-        this.height =   this.spriteHeight / 2.5;
+        this.spriteWidth= 218;
+        this.spriteHeight = 177;
+        this.width = this.spriteWidth / 2;
+        this.height =   this.spriteHeight / 2;
         this.x = Math.random() * (canvas.width - this.width);
         this.y = Math.random() * (canvas.height - this.height);
+        this.newX = Math.random() * canvas.width;
+        this.newY = Math.random() * canvas.height;
         this.frame = 0;
         this.flapSpeed = Math.floor(Math.random() * 3 + 1);
-        this.angle = 0;
-        this.angleSpeed = Math.random() * 0.2;
-        this.curve = Math.random() * 7;
+        this.interval = Math.floor(Math.random() * 200 + 50);
     }
     update(){
-        this.x -= this.speed;
-        this.y += this.curve * Math.sin(this.angle);
-        this.angle += this.angleSpeed; 
-        //this.y+=  Math.random() * 5 - 2.5;
+        if(gameFrame % this.interval === 0 ) {
+            this.newX = Math.random() * (canvas.width - this.width); 
+            this.newY = Math.random() * (canvas.height - this.height); 
+        }
+       
+        let dx = this.x - this.newX;
+        let dy = this.y - this.newY;
+        this.x -= dx/70;
+        this.y -= dy/70;
+        
         if(this.x + this.width < 0 ) this.x = canvas.width;
+
         //animate sprites
         gameFrame % this.flapSpeed === 0 ? this.frame > 4 ? this.frame = 0 : this.frame++ : 0;
     }
