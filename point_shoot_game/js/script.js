08a09e45112e8b1c8c6557ca6f816619f1621f75
Sros27 <84526104+SaraRosad@users.noreply.github.com>
2022-11-15 19:30:53 -0400

    11-15-2022

--- point_shoot_game/js/script.js
+++ point_shoot_game/js/script.js
@@ -9,31 +9,46 @@ let ravens = [];
 
 class Raven {
     constructor(){
-        this.width = 100;
-        this.height = 50;
+        this.spriteWidth = 340;
+        this.spriteHeight = 200;
+        this.sizeModifier = Math.random() * 0.6 + 0.4;
+        this.width =  this.spriteWidth *  this.sizeModifier;
+        this.height = this.spriteHeight *  this.sizeModifier;
         this.x = canvas.width;
         this.y = Math.random() * (canvas.height - this.height);
         this.directionX = Math.random() * 5 + 3;
-        this.directionY = Math.random() * 5 -2.5;
+        this.directionY = Math.random() * 5 - 2.5;
         this.markedForDeletion = false;
         this.image = new Image();
-        this.image.src = 'images/raven.png';
-        this.spriteWidth = 271;
-        this.spriteHeight = 194;
+        this.image.src = 'images/boom.png';
+        this.frame = 0;
+        this.maxFrame = 4;
+        this.timeSinceFlap = 0;
+        this.flapInterval = Math.random * 50 + 50;
     }
 
-    update(){
+    update(deltatime){
+        if(this.y < 0 || this.y > canvas.height - this.height){
+            this.directionY = this.directionY * -1;
+        }
         this.x -= this.directionX;
-        if(this.x < 0 - this.width)this.markedForDeletion =true;
+        this.y += this.directionY
+        if(this.x < 0 - this.width)this.markedForDeletion = true;
+        this.timeSinceFlap += deltatime;
+        if(this.timeSinceFlap > this.flapInterval){
+            if(this.frame > this.maxFrame)this.frame = 0;
+            else this.frame++;
+            this.timeSinceFlap = 0;    
+        }
     }
     draw(){
-        ctx.fillRect(this.x, this.y, this.width, this.height);
-        ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
+        ctx.strokeRect(this.x, this.y, this.width, this.height);
+        ctx.drawImage(this.image, 0, 0, this.frame * this.spriteWidth, this.spriteHeight, this.x, this.y, this.width, this.height);
     }
 }
 
 function animate(timestamp){
-    ctx.clearRect(0,0, canvas.width, canvas.height);
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
     let deltatime = timestamp - lastTime;
     lastTime = timestamp;
     timeToNextRaven += deltatime;
@@ -43,7 +58,7 @@ function animate(timestamp){
         timeToNextRaven = 0;
     }
 
-    [...ravens].forEach(object => object.update());
+    [...ravens].forEach(object => object.update(deltatime));
     [...ravens].forEach(object => object.draw());
     ravens = ravens.filter(object => !object.markedForDeletion);
 
